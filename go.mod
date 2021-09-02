da06b185d3eaf9d22cfb093d67730180725a0b0a
Carlos Zimmerle <carloszimmerle@gmail.com>
2021-09-02 01:41:57 -0300

    feat: add filters, stopwords, and stemmer

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/carloszimm/stack-mining
 
 go 1.17
 
-require github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
+require (
+	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/bbalet/stopwords v1.0.0
+	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
+	github.com/reiver/go-porterstemmer v1.0.1
+)
+
+require (
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- mining.go
+++ mining.go
@@ -6,8 +6,13 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/bbalet/stopwords"
 	"github.com/gocarina/gocsv"
+	"github.com/reiver/go-porterstemmer"
 )
 
 var filesPath string = filepath.Join("..", "..", "Data Explorer", "31-08-2021", "rxjs")
@@ -18,6 +23,10 @@ type Post struct {
 	CreationDate string `csv:"-"`
 }
 
+func init() {
+	stopwords.LoadStopWordsFromFile("stopwords.txt", "en", "\n")
+}
+
 func main() {
 	files, err := ioutil.ReadDir(filesPath)
 	if err != nil {
@@ -25,32 +34,32 @@ func main() {
 	}
 
 	var count = 0
-	c := make(chan map[int]string)
+	c := make(chan []*Post)
 
 	for i, f := range files {
 		go readCSV(filepath.Join(filesPath, f.Name()), c)
 		count = i
 	}
 
-	var resultPosts []map[int]string
+	var resultPosts [][]*Post
 
 	for i := 0; i < count+1; i++ {
 		posts := <-c
 		resultPosts = append(resultPosts, posts)
 	}
 
-	mergedPosts := mergeMaps(resultPosts)
+	mergedPosts := mergeArrays(resultPosts)
 
-	fmt.Println(len(mergedPosts))
+	fmt.Println("Total questions:", len(mergedPosts))
 }
 
-func mergeMaps(maps []map[int]string) map[int]string {
+func mergeArrays(postsArray [][]*Post) map[int]string {
 	result := make(map[int]string)
 
-	for _, mapPosts := range maps {
-		for id, body := range mapPosts {
-			if _, ok := result[id]; !ok {
-				result[id] = body
+	for _, posts := range postsArray {
+		for _, post := range posts {
+			if _, ok := result[post.Id]; !ok {
+				result[post.Id] = post.Body
 			}
 		}
 	}
@@ -58,7 +67,7 @@ func mergeMaps(maps []map[int]string) map[int]string {
 	return result
 }
 
-func readCSV(path string, c chan map[int]string) {
+func readCSV(path string, c chan []*Post) {
 	postsFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
 
 	if err != nil {
@@ -72,11 +81,48 @@ func readCSV(path string, c chan map[int]string) {
 		panic(err)
 	}
 
-	m := make(map[int]string)
+	cleanText(&posts)
 
-	for _, post := range posts {
-		m[post.Id] = post.Body
+	c <- posts
+}
+
+func cleanText(posts *[]*Post) {
+
+	// puctuation
+	puctReg := regexp.MustCompile(`\p{P}`)
+	spaceReg := regexp.MustCompile(`\s+`)
+
+	for _, post := range *posts {
+		// load the HTML document
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(post.Body))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// remove code, pre, and blockquotes tags
+		doc.Find("code, pre, blockquotes").Remove()
+
+		// get text from html
+		text := doc.Find("body").Text()
+
+		text = stopwords.CleanString(text, "en", false)
+
+		// remove puctuation and extra space
+		text = strings.TrimSpace(spaceReg.ReplaceAllString(puctReg.ReplaceAllString(text, ""), " "))
+
+		text = stem(text)
+
+		post.Body = text
+	}
+}
+
+func stem(text string) string {
+	textSplitted := strings.Split(text, " ")
+	text = ""
+
+	for _, textPart := range textSplitted {
+		text += " " + porterstemmer.StemString(textPart)
 	}
 
-	c <- m
+	return strings.TrimSpace(text)
 }
